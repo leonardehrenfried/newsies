28d851da8c31a399d4171cd11f6038464a53ee82
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2011-06-08 16:32:19 +0200

    cufon working

--- index.php
+++ index.php
@@ -10,18 +10,26 @@ if ( is_date() ) { ?><p id="archive-what">You are browsing the archive for <?php
 
 while ( have_posts() ) : the_post(); ?>
 
-<div class="post">
+<article class="post">
 <h2 class="entry-title"><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
-<?php if ( is_new_day() ) { ?><p class="meta"><?php the_date()?></p><?php } ?>
+
+<?php if ( is_new_day() ) { ?>
+  <p class="date"><?php the_date()?></p>
+<?php } ?>
+
 <div class="entry-content">
 <?php the_content()?>
 </div>
-<?php if ( comments_open() ) : ?>
-<p class="meta"><?php comments_popup_link(); ?></p>
-<?php endif; ?> 
-</div>
 
+<?php if ( comments_open() ) : ?>
+  <p class="meta"><?php comments_popup_link(); ?></p>
+<?php endif; ?> 
+
+</article>
+
+<div class="hr"></div>
 <?php endwhile; ?>
+
 <?php if (show_posts_nav()) : ?>
 		<div class="navigation">
 			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
