bdc06e241e8ab152ee01ec1cd52a0f0371a36239
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2011-06-08 16:56:25 +0200

    single page view

--- single.php
+++ single.php
@@ -1,16 +1,19 @@
 <?php get_header()?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="entry">
-<h2 class="entry-title"><?php the_title()?></h2>
-<p class="meta">Published on <?php the_date()?></p>
-<div class="entry-content">
-<?php the_content()?>
-<p class="pagelink"><?php wp_link_pages('pagelink=Page %&before=&after='); ?></p>
-</div>
-<p class="meta">Filed under: <?php the_category(', '); ?><?php the_tags('<br />Tags: '); ?></p>
-<?php comments_template()?>
-</div>
+<article class="post">
+  <h2 class="entry-title"><?php the_title()?></h2>
+  <p class="date"><?php the_date()?></p>
+  <div class="entry-content">
+  
+    <?php the_content()?>
+    <p class="pagelink"><?php wp_link_pages('pagelink=Page %&before=&after='); ?></p>
+
+  </div>
+
+  <p class="meta">Filed under: <?php the_category(', '); ?><?php the_tags('<br />Tags: '); ?></p>
+  <?php comments_template()?>
+</article>
 <?php endwhile; ?>
 		<div class="navigation">
 			<div class="alignleft"><?php previous_post_link('&laquo; %link') ?></div>
