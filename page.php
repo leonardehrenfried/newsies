848e00dc1430b4c7df298596e40cfe34fd32f5f9
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2011-06-09 20:06:59 +0200

    page layout same as post

--- page.php
+++ page.php
@@ -1,14 +1,14 @@
 <?php get_header()?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="entry">
-<h2 class="entry-title"><?php the_title()?></h2>
-<div class="entry-content">
-<?php the_content()?>
-</div>
-<?php if ( comments_open() ) : ?>
-<?php comments_template()?>
-<?php endif; ?> 
+<div class="post">
+  <h2 class="entry-title"><?php the_title()?></h2>
+  <div class="entry-content">
+    <?php the_content()?>
+  </div>
+  <?php if ( comments_open() ) : ?>
+    <?php comments_template()?>
+  <?php endif; ?> 
 </div>
 <?php endwhile; endif; ?>
 
