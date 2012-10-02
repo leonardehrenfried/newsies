8b257b9db598f71fa6549d833ef6dc52d1799f32
Leonard Ehrenfried <leonard.ehrenfried@commercetools.de>
2012-10-02 16:22:31 +0200

    Add Influads

--- header.php
+++ header.php
@@ -25,6 +25,8 @@
 
   <div id="frame">
 
+  <div id='influads_block' class='influads_block'></div><script type='text/javascript'>(function(){var acc='acc_963d701_pub';var st='css';var or='130';var e=document.getElementsByTagName('script')[0];var d=document.createElement('script');d.src=('https:' == document.location.protocol ?'https://' : 'http://') +'engine.influads.com/show/'+or+'/'+st+'/'+acc;d.type='text/javascript';d.async=true;d.defer=true; e.parentNode.insertBefore(d,e);})();</script>
+
   <nav>
     <ul>
       <?php wp_list_pages('title_li=&depth=1');?>
